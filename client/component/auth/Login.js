9e2636a31737f63a6f126d049632d843c8f68a30
Chandni <chandnissirohi@gmail.com>
2020-01-27 23:23:44 +0530

    add login integration

--- client/component/auth/Login.js
+++ client/component/auth/Login.js
@@ -1,21 +1,69 @@
 import React from "react";
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const username = this.state.username;
+    const email = this.state.email;
+    const password = this.state.password;
+
+    const userData = { username, email, password };
+
+    fetch("/api/v1/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData)
+    })
+      .then(res => res.json())
+      .then(user => console.log(user, "User has logged in"));
+  };
+
   render() {
     return (
       <div>
-       
-          <h1>Login</h1>
-          <label>Username :</label>
-          <input type="text" placeholder="Please enter your username" />
-          <br />
-          <label>Email Id :</label>
-          <input type="email" placeholder="Please enter your email" />
-          <br />
-          <label>Password :</label>
-          <input type="password" placeholder="Please enter your password" />
-          <br />
-          <button className="login">Login</button>
+        <h1>Login</h1>
+        <label>Username :</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Please enter your username"
+          onChange={this.handleChange}
+        />
+        <br />
+        <label>Email Id :</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Please enter your email"
+          onChange={this.handleChange}
+        />
+        <br />
+        <label>Password :</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Please enter your password"
+          onChange={this.handleChange}
+        />
+        <br />
+        <button onClick={this.handleSubmit} className="login">
+          Login
+        </button>
       </div>
     );
   }
--- client/index.js
+++ client/index.js
@@ -3,16 +3,16 @@ import ReactDom from "react-dom";
 
 import "./stylesheet/main.css";
 // import Quiz from "./component/Quiz";
-// import Login from "./component/auth/Login";
-import Signup from './component/auth/Signup'
+import Login from "./component/auth/Login";
+// import Signup from './component/auth/Signup'
 // import AdminLogin from './component/auth/AdminLogin'
 
 function App() {
   return (
     <div className="App wrapper">
       {/* <Quiz /> */}
-      {/* <Login /> */}
-      <Signup />
+      <Login />
+      {/* <Signup /> */}
       {/* <AdminLogin /> */}
     </div>
   );
