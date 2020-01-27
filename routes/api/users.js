3fa1e2bb100debb6a869bb7d98f68648084f8740
Chandni <chandnissirohi@gmail.com>
2020-01-27 15:19:00 +0530

    update: auth flow done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,33 +1,36 @@
-var express = require('express');
-const User = require("../../models/user")
-const jwt = require("jsonwebtoken")
+var express = require("express");
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 const auth = require("../../modules/auth");
 var router = express.Router();
 
-// User registration
+// User signup
 router.post("/signup", (req, res, next) => {
   User.create(req.body, (err, User) => {
-    if(err) return next(err)
-    if(!User) return res.json({message: "User not found", success: false})
-    res.json({User, success: true, })
-  })
-})
+    if (err) return next(err);
+    if (!User) return res.json({ message: "User not found", success: false });
+    console.log(User, "user created");
+    res.json({ User, success: true });
+  });
+});
 
 // User login
 router.post("/login", (req, res, next) => {
-  let{email, password} = req.body
-  User.findOne({ email}, (err, user) => {
-    if(err) return next(err)
-    if(!user) return res.json({success: false, message: "Invalid email"})
+  let { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err) return next(err);
+    if (!user) return res.json({ success: false, message: "Invalid email" });
     user.verifyPassword(password, (err, matched) => {
-      if(err) return next(err);
-      if(!matched)
-      return res.status(422).json({success: false, message: "Invalid Password"})
+      if (err) return next(err);
+      if (!matched)
+        return res
+          .status(422)
+          .json({ success: false, message: "Invalid Password" });
 
       // jwt auth
 
       jwt.sign(
-        { 
+        {
           userid: user._id,
           username: user.username,
           email: user.email,
@@ -35,49 +38,47 @@ router.post("/login", (req, res, next) => {
         },
         "secret",
         (err, token) => {
-          if(err) return next(err)
-          res.json({success: true, message: "You are now logged in", token });
+          if (err) return next(err);
+          res.json({ success: true, message: "You are now logged in", token });
         }
-      )
-    })
-  })
-})
+      );
+    });
+  });
+});
 
 // get user's listings
 
-router.get("/", (req, res, next)=>{
-  user.find({}, "-password", (err, users) => {
-    if(err) return next(err);
-    if(!users)
-    return res.json({ success: false, message: "Users not found "})
-    res.json({users, success: true})
-  })
-})
+router.get("/", (req, res, next) => {
+  User.find({}, "-password", (err, users) => {
+    if (err) return console.log(err, "err");
+    if (!users)
+      return res.json({ success: false, message: "Users not found " });
+    res.json({ users, success: true });
+  });
+});
 
 // get single user
 
 router.get("/:id", (req, res, next) => {
-  const id = req.params.id
-  user.findById(id, "-password", (err, user) =>{
-    if(err) return next(err)
-    if(!user) return res.json({success: false, message: "User not found"})
-    res.json({user, success: true})
-  })
-})
+  const id = req.params.id;
+  User.findById(id, "-password", (err, user) => {
+    if (err) return res.json(err);
+    if (!user) return res.json({ success: false, message: "User not found" });
+    res.json({ user, success: true });
+  });
+});
 
-router.use(auth.verifyToken)
+router.use(auth.verifyToken);
 
 // update user
 
-router.put("/:id", (req, res, next) =>{
-  const id = req.params.id
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
   user.findByIdAndUpdate(id, req.body, { new: true }, (err, user) => {
-    if (err) return next(err)
-    if(!user) return res.json({success: false, message: "User not found"})
-    res.json({user, success: true})
-  })
-})
-
-
+    if (err) return next(err);
+    if (!user) return res.json({ success: false, message: "User not found" });
+    res.json({ user, success: true });
+  });
+});
 
 module.exports = router;
