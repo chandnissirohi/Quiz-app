68adb084de985cc1afb5485eb8ba1e0db4ca410e
Chandni <chandnissirohi@gmail.com>
2020-01-28 13:43:49 +0530

    add quiz component

--- client/component/App.js
+++ client/component/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import "../stylesheet/main.css";
-// import Quiz from "./component/Quiz";
+import Quiz from "./Quiz";
 import UserLogin from "./auth/UserLogin";
 import UserSignup from "./auth/UserSignup";
 import AdminLogin from "./auth/AdminLogin";
@@ -15,16 +15,14 @@ import AdminLogin from "./auth/AdminLogin";
 class App extends React.Component {
   render() {
     return (
-      // <div className="App">
-
-      <>
-        {/* <AdminLogin /> */}
+      <div className="App">
         <Switch>
           <Route exact path="/admins/login" component={AdminLogin} />
           <Route exact path="/user/signup" component={UserSignup} />
           <Route exact path="/user/login" component={UserLogin} />
+          <Route exact path="/quiz" component={Quiz} />
         </Switch>
-      </>
+      </div>
     );
   }
 }
