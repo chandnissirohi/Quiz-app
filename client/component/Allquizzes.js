e9e82679b8869c3a0b4cb5167e0c132a7e0216b1
Chandni <chandnissirohi@gmail.com>
2020-01-30 22:50:02 +0530

    add UI for all quiz titiles

--- client/component/Allquizzes.js
+++ client/component/Allquizzes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AdminHeader from "./AdminHeader.js";
 
 class Allquizzes extends React.Component {
   constructor() {
@@ -21,15 +22,27 @@ class Allquizzes extends React.Component {
   render() {
     // console.log(this.state);
     return (
-      <div>
-        <h1>All Quizzes</h1>
-        {this.state.allquizzes &&
-          this.state.allquizzes.map(quiz => (
-            <div className="quiz-selector">
-              <Link to={`/admin/quiz/new/${quiz._id}`}>{quiz.quizTitle}</Link>
-            </div>
-          ))}
-      </div>
+      <>
+        <AdminHeader />
+        <div className="wrapper">
+          <center>
+            <h1 className="login-header">All Quizzes</h1>
+          </center>
+          <div className="field">
+            {this.state.allquizzes &&
+              this.state.allquizzes.map(quiz => (
+                <div className="quiz-selector control">
+                  <Link
+                    className="button is-text"
+                    to={`/admin/quiz/new/${quiz._id}`}
+                  >
+                    {quiz.quizTitle}
+                  </Link>
+                </div>
+              ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
