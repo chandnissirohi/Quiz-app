62469db34d2271c9db0a0ac080666e4f1d7c17f9
Chandni <chandnissirohi@gmail.com>
2020-02-03 01:09:44 +0530

    refactor quiz router and add quiz controller

--- routes/api/quiz.js
+++ routes/api/quiz.js
@@ -1,76 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const quizController = require("../../controllers/quiz")
 const jwt = require("jsonwebtoken");
 
 const Quiz = require("../../models/quiz");
 const Admin = require("../../models/admin");
 
-// finding all Quiz
 
-router.get("/", (req, res) => {
-  Quiz.find({}, (err, quizzes) => {
-    if (err) return res.json({ err });
-    res.json({ quizzes, success: true });
-  });
-});
+router.get("/", quizController.list);
 
-//  create a quiz
+router.post("/create", quizController.create);
 
-router.post("/create", (req, res) => {
-  Quiz.create(req.body, (err, createdQuiz) => {
-    if (err) return res.json({ err });
-    res.json({ createdQuiz, success: true, message: "Quiz Created Succesfully"});
-  });
-});
+router.get("/:id", quizController.singleQuiz);
 
-//  find one Quiz
+router.put("/update", quizController.update);
 
-router.get("/:id", (req, res) => {
-  Quiz.findById({ _id: req.params.id }, (err, quiz) => {
-    if (err) return next({ err });
-    res.json({ quiz, success: true });
-  });
-});
-
-// edit Quiz
-
-router.put("/update", (req, res) => {
-  console.log(req.body , "req in /update");
-  const quizId = req.body.quizId;
-  Quiz.findById(quizId, (err, quiz) => {
-    if (err) return next({ err });
-    console.log(quiz);
-    const questionSet = [...quiz.questionSet, req.body.question._id];
-    const totalScore = quiz.totalScore + 1;
-
-    Quiz.findByIdAndUpdate(
-      quizId,
-      { questionSet, totalScore },
-      { new : true },
-      (err, updatedQuiz) => {
-        if (err) return next({err});
-        // console.log(updatedQuiz);
-        res.json({
-          updatedQuiz,
-          success: true,
-          message: "Quiz updated Succesfully"
-        });
-      }
-    );
-  });
-});
-
-// delete Quiz
-
-router.delete("/:id", (req, res) => {
-  Quiz.findByIdAndDelete(req.params.id, (err, deletedQuiz) => {
-    if (err) return res.json({ err });
-    res.json({
-      deletedQuiz,
-      success: true,
-      message: "Quiz Deleted Succesfully"
-    });
-  });
-});
+router.delete("/:id", quizController.delete);
 
 module.exports = router;
